6509d23948ec8633139b7922d2c5334f3fdb8017
sroff82 <mateotorrez76@gmail.com>
2026-06-22 16:17:46 -0600

    Refactor Usuario class for global exposure
    Removed ESM export syntax and exposed Usuario class globally.

--- models/usuario.js
+++ models/usuario.js
@@ -1,12 +1,13 @@
-export default class Usuario {
+// models/usuario.js
 
-constructor(nombre, rol, campo, password) {
-
-this.nombre = nombre;
-this.rol = rol;
-this.campo = campo;
-this.password = password;
-
+class Usuario {
+    constructor(nombre, rol, campo, password) {
+        this.nombre = nombre;
+        this.rol = rol;
+        this.campo = campo;
+        this.password = password;
+    }
 }
 
-}
+// Se expone el modelo al contexto global eliminando la sintaxis ESM (import/export)
+window.Usuario = Usuario;
